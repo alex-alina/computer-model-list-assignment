e0a9389282265392a9dc4d916efefca77c568520
Alina <alex.alina.rusu@gmail.com>
2018-11-03 13:26:11 +0100

    Step 1 - Render some elements- done

--- src/App.js
+++ src/App.js
@@ -1,15 +1,42 @@
 import React, { Component } from 'react';
-import { Provider } from 'react-redux'
-import store from './store'
+
 
 class App extends Component {
+
+  state = {
+    "Ivel Z3": {
+      manufacturer: "Ivasim",
+      year: 1969,
+      origin: "Croatia"
+    },
+    "Bally Astrocade": {
+      manufacturer: "Bally Consumer Products",
+      year: 1977,
+      origin: "USA"
+    },
+    "Sord M200 Smart Home Computer": {
+      manufacturer: "Sord Computer Corporation",
+      year: 1971,
+      origin: "Japan"
+    },
+    "Commodore 64": {
+      manufacturer: "Commodore",
+      year: 1982,
+      origin: "USA"
+    }
+  }
+
   render() {
     return (
-      <Provider store={store}>
-        <div className="App">
-         
-        </div>
-      </Provider>
+      <div className="App">
+        <select>
+          <option value="">-- pick a model --</option>
+          {Object.keys(this.state).map(computer =>
+            <option key={computer} value={computer}>{computer} ({this.state[computer].year})</option>
+          )}
+        </select>
+      </div>
+
     );
   }
 }
