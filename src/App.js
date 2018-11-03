86224381e2f4d23f9eb9747ef94606a06ab1d261
Alina <alex.alina.rusu@gmail.com>
2018-11-03 14:33:29 +0100

    repaired bug in Step2 - separated computers list from selected value

--- src/App.js
+++ src/App.js
@@ -4,25 +4,27 @@ import React, { Component } from 'react';
 class App extends Component {
 
   state = {
-    "Ivel Z3": {
-      manufacturer: "Ivasim",
-      year: 1969,
-      origin: "Croatia"
-    },
-    "Bally Astrocade": {
-      manufacturer: "Bally Consumer Products",
-      year: 1977,
-      origin: "USA"
-    },
-    "Sord M200 Smart Home Computer": {
-      manufacturer: "Sord Computer Corporation",
-      year: 1971,
-      origin: "Japan"
-    },
-    "Commodore 64": {
-      manufacturer: "Commodore",
-      year: 1982,
-      origin: "USA"
+    compModels : {
+      "Ivel Z3": {
+        manufacturer: "Ivasim",
+        year: 1969,
+        origin: "Croatia"
+      },
+      "Bally Astrocade": {
+        manufacturer: "Bally Consumer Products",
+        year: 1977,
+        origin: "USA"
+      },
+      "Sord M200 Smart Home Computer": {
+        manufacturer: "Sord Computer Corporation",
+        year: 1971,
+        origin: "Japan"
+      },
+      "Commodore 64": {
+        manufacturer: "Commodore",
+        year: 1982,
+        origin: "USA"
+      }
     }
   }
   
@@ -39,8 +41,8 @@ class App extends Component {
       <div className="App">
         <select value={this.state.selectedValue} onChange={this.updateSelection}>
           <option value="">-- pick a model --</option>
-          {Object.keys(this.state).map(computer =>
-            <option key={computer} value={computer}>{computer} ({this.state[computer].year})</option>
+          {Object.keys(this.state.compModels).map(computer =>
+            <option key={computer} value={computer}>{computer} ({this.state.compModels[computer].year})</option>
           )}
         </select>
         {console.log(this.state)}
